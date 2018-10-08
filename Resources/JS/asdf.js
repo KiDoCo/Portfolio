602d12528c8b7c82bbd9e412bbc206b24c465423
Tuominen Joonas TTK17SP <joonastuominen@kamk.fi>
2018-10-08 14:20:54 +0300

    ADD 2d canvas drawing logic

--- Resources/JS/asdf.js
+++ Resources/JS/asdf.js
@@ -1,19 +1,31 @@
-main();
+var canvas =	document.getElementById("glcanvas");
+const gl = canvas.getContext("2d");
 
+render();
+var x = -150;
+var y = -200;
+var testx = 0;
+var testy = 0;
 function main()
-{
-	var canvas = document.getElementById("glcanvas");
-	
-	const gl = canvas.getContext("webgl");
-	
-	
-	if(gl == null)
-	{
-		alert("DAFUQ DUUUDE");
-		return;
-	}
-	
-	gl.clearColor(0.0,255.0,0.0,1.0);
-	
-	gl.clear(gl.COLOR_BUFFER_BIT);
+{	
+	gl.rotate(5*Math.PI/180);
+draw();	
+x += 1;
+y += 1;
 }
+
+function render()
+{
+	
+	main();
+	requestAnimationFrame(render);
+}
+
+function draw()
+{
+
+	gl.strokeStyle = "#FF0000";
+	gl.strokeRect(x,x,y,y);
+}
+
+
