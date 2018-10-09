a3e6acb7942da63ac99a32a5c8a8516612e7b7e9
Tuominen Joonas TTK17SP <joonastuominen@kamk.fi>
2018-10-09 08:50:15 +0300

    ADD circle drawing algorithm

--- Resources/JS/asdf.js
+++ Resources/JS/asdf.js
@@ -1,31 +1,41 @@
 var canvas =	document.getElementById("glcanvas");
 const gl = canvas.getContext("2d");
-
+save();
 render();
-var x = -150;
-var y = -200;
-var testx = 0;
-var testy = 0;
+var x = 40;
+var y = 50;
+var startx = 0;
 function main()
 {	
-	gl.rotate(5*Math.PI/180);
-draw();	
-x += 1;
-y += 1;
+	gl.translate(canvas.width/2,canvas.height/2);
+	gl.rotate(15 * Math.PI / 180)
+	draw();	
 }
 
 function render()
 {
-	
 	main();
 	requestAnimationFrame(render);
+	startx += 1;
+	gl.translate(-canvas.width/2,-canvas.height/2);
+	if(startx % 100 == 0 && startx != 0)
+	{
+		startx = 0;
+		gl.clearRect(0,0,canvas.width,canvas.height);
+			gl.resetTransform();
+	}
+
 }
 
 function draw()
 {
 
-	gl.strokeStyle = "#FF0000";
-	gl.strokeRect(x,x,y,y);
+	gl.strokeStyle = "#00FF00";
+	gl.strokeRect(100,100,x,y);
 }
 
+function save()
+{
+	gl.save();
+}
 
