f13fe16200a9e58b41a1b634171d792b9135f346
Tuominen Joonas TTK17SP <joonastuominen@kamk.fi>
2018-10-22 16:36:39 +0300

    ADD three.js and webgl basic stuff

--- Resources/JS/PageGL.js
+++ Resources/JS/PageGL.js
@@ -1,14 +1,14 @@
-var canvas = document.getElementById("glcanvas");
-const gl = canvas.getContext("2d");
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+var canvas = document.getElementsByClassName("glcontainer");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-gl.resetTransform();
-gl.fillStyle = '#FF0000';
-gl.fillRect(0,0, 1920,1080);
-var style = window.getComputedStyle(document.getElementById('bleh'),null).getPropertyValue('font-size');
+var style = 16;
 var initvalue = parseFloat(style);
-document.getElementById("bleh").style.fontSize = (initvalue + window.innerWidth / 150) + 'px';
-
+$("span").css("font-size" ,(initvalue + window.innerWidth / 150) + 'px');
+init();
+render();
 
 
 function readTextFile(file)
@@ -45,5 +45,49 @@ $(window).resize(function()
 	canvas.height = window.innerHeight;
 	canvas.width = window.innerWidth;
 	//Calculate the font size
-	document.getElementById("bleh").style.fontSize = (initvalue + window.innerWidth / 150) + 'px';
+	if((initvalue + window.innerWidth /150) <= 32)
+	{
+		$('span').css("font-size" , (initvalue + window.innerWidth / 150) + 'px');
+	}
+	else
+	{
+		$('span').css("font-size" , 32 + 'px');
+	}
+
 });
+
+function init()
+{
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 1;
+
+	scene = new THREE.Scene();
+
+	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	material = new THREE.MeshNormalMaterial();
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	$(".glcontainer").append(renderer);
+
+
+}
+
+
+function render()
+{
+
+	requestAnimationFrame( render );
+
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
+
+}
+
