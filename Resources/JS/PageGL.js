6ff4a2a00e78ad9e6bd11a16d0f5799b379c2af5
Tuominen Joonas TTK17SP <joonastuominen@kamk.fi>
2018-11-26 14:09:50 +0200

    ADD letter i and k

--- Resources/JS/PageGL.js
+++ Resources/JS/PageGL.js
@@ -59,20 +59,104 @@ $(window).resize(function()
 
 });
 
+function K()
+{
+	var geometry = new THREE.Geometry();
 
+	geometry.vertices.push
+	(
+		//straight palkki
+		new THREE.Vector3(-5,20,0), //mid left 0
+		new THREE.Vector3(-5,-10,0), //down left 1
+		new THREE.Vector3(10,-10,0), //down right 2
+		new THREE.Vector3(10,20,0),  //mid right 3
+		new THREE.Vector3(10,50,0),	 //Top right 4
+		new THREE.Vector3(-5,50,0),  //top left 5
+		//two side palliki
+		new THREE.Vector3(30,50,0), //top 6
+		new THREE.Vector3(40,50,0), //top 7
+		new THREE.Vector3(20,20,0),  //mid 8
+		new THREE.Vector3(40,-10,0), //down 9
+		new THREE.Vector3(30,-10,0),  //down 10
+		new THREE.Vector3(10,25,0),
+		new THREE.Vector3(10,15,0)
+	);
+
+	geometry.faces.push
+	(
+		new THREE.Face3(0,1,2), //down square
+		new THREE.Face3(0,2,3), 
+
+		new THREE.Face3(1,3,5), //upper square
+		new THREE.Face3(3,4,5),
+
+		new THREE.Face3(12,8,11), //mid point intersect points
+
+
+		new THREE.Face3(11,8,6), //up
+		new THREE.Face3(7,6,8),
+
+		new THREE.Face3(10,9,8), //down
+		new THREE.Face3(8,12,10)
+	);
+
+	geometry.computeBoundingSphere();
+
+	return geometry;
+}
+
+function i()
+{
+	//the base of i
+	var geom = new THREE.Geometry();
+
+	geom.vertices.push
+	(
+		//straight palkki
+		new THREE.Vector3(-5,20,0), //mid left 0
+		new THREE.Vector3(-5,-10,0), //down left 1
+		new THREE.Vector3(10,-10,0), //down right 2
+		new THREE.Vector3(10,20,0),  //mid right 3
+		new THREE.Vector3(10,50,0),	 //Top right 4
+		new THREE.Vector3(-5,50,0),  //top left 5
+	);
+
+	geom.faces.push
+	(
+
+		new THREE.Face3(0,1,2), //down square
+		new THREE.Face3(0,2,3), 
+
+		new THREE.Face3(1,3,5), //upper square
+		new THREE.Face3(3,4,5),
+	);
+
+	//create point for i
+	var geom2 = new THREE.CircleGeometry(8,32);
+	geom2.translate(2.5,65,0);
+	//adding meshes so we can use matrix calculations
+	var mesh = (geom, new THREE.MeshBasicMaterial({color:0x0f0f0f}));
+	var mesh2 = (geom2, new THREE.MeshBasicMaterial({color:0x0f0f0f}));
+
+	var singleGeometry = new THREE.Geometry();
+
+	singleGeometry.merge(geom,mesh.matrix);
+	singleGeometry.merge(geom2,mesh2.matrix);
+
+	return singleGeometry;
+
+}
 
 function init()
 {
 	renderer = new THREE.WebGLRenderer();
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.01, 1000 );
-	camera.position.z = 27;
+	camera.position.z = 130;
+	camera.position.x = 20;
+	camera.position.y = 30;
 
 
-	var light = new THREE.PointLight( 0xffffff);
-	light.position.copy(camera.position);
-
-
-	mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0x00ffff}));
+	mesh = new THREE.Mesh(i(), new THREE.MeshBasicMaterial({color: 0x00ffff}));
 	scene = new THREE.Scene();
 
 	scene.add(mesh);
@@ -87,7 +171,7 @@ function init()
 function render()
 {
 	requestAnimationFrame( render );
-
+	mesh.rotation.z = startx;
 	if(startx < -1.0)
 	{
 		increase = true;
