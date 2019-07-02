c8dff2d7d90d33dc185939ad772067039b5ace5e
KiDoCo <joonastu97@gmail.com>
2019-07-02 10:59:16 +0300

    Fixed the layout

--- Resources/JS/PageGL.js
+++ Resources/JS/PageGL.js
@@ -1,13 +1,26 @@
-var camera, scene, renderer;
-var geometry, material, mesh;
+
 var startx = 0;
 var increase = false;
 var boxarray = [];
 var style = 16;
 var initvalue = parseFloat(style);
-var canvas = document.getElementsByClassName("glcontainer");
-init();
-render();
+var G_gl;
+main();
+
+function main()
+{
+	ChangeHTMLOf('#textWindow','MainPage.html');
+	const canvas = document.querySelector("#glcontainer");
+	const gl = canvas.getContext("webgl");
+	if(gl == null)
+	{
+		alert("Unable to initialize WebGL");
+		return;
+	}
+	G_gl = gl;
+	gl.clearColor(0.5,0.0,0.0,1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
 
 
 function readTextFile(file)
@@ -31,172 +44,35 @@ function readTextFile(file)
 	}
 	rawFile.send(null);
 }
-//$.ajax({url: "../Text/bleh.txt", success: function(result){
-//$("#test").html(result);
-//}});
+
+
+
+function resize(canvas)
+{
+	var displayWidth = canvas.clientWidth;
+var displayHeight = canvas.clientHeight;
+if(canvas.width != displayWidth || canvas.height != displayHeight)
+{
+	canvas.width = displayWidth;
+	canvas.height = displayHeight;
+}
+}
+
+
 
 
 
 //event when resizes occurs
-$(window).resize(async function()
+$(window).resize(function()
 {
-	canvas[0].height = window.innerHeight;
-	canvas[0].width = window.innerWidth;
-});
+
+	G_gl.height = canvas.clientHeight;
+	G_gl.width = canvas.clientWidth;
+
+	});
 
 function ChangeHTMLOf(source,target)
 {
 	$(source).load(target);
-}
-
-function K()
-{
-	var geometry = new THREE.Geometry();
-
-	geometry.vertices.push
-	(
-		//straight palkki
-		new THREE.Vector3(-5,20,0), //mid left 0
-		new THREE.Vector3(-5,-10,0), //down left 1
-		new THREE.Vector3(10,-10,0), //down right 2
-		
-		new THREE.Vector3(10,20,0),  //mid right 3
-		new THREE.Vector3(10,50,0),	 //Top right 4
-		new THREE.Vector3(-5,50,0),  //top left 5
-		//two side palliki
-		new THREE.Vector3(30,50,0), //top 6
-		new THREE.Vector3(40,50,0), //top 7
-		new THREE.Vector3(20,20,0),  //mid 8
-		
-		new THREE.Vector3(40,-10,0), //down 9
-		new THREE.Vector3(30,-10,0),  //down 10
-		new THREE.Vector3(10,25,0),   //mid section 11
-		
-		new THREE.Vector3(10,15,0),   //12
-		
-	);
-
-	geometry.faces.push
-	(
-		new THREE.Face3(0,1,2), //down square
-		new THREE.Face3(0,2,3), 
-
-		new THREE.Face3(1,3,5), //upper square
-		new THREE.Face3(3,4,5),
-
-		new THREE.Face3(12,8,11), //mid point intersect points
-
-
-		new THREE.Face3(11,8,6), //up
-		new THREE.Face3(7,6,8),
-
-		new THREE.Face3(10,9,8), //down
-		new THREE.Face3(8,12,10),
-		//backface
-		new THREE.Face3(2,1,0),
-		new THREE.Face3(3,2,0),
-		new THREE.Face3(5,3,1),
-		new THREE.Face3(5,4,3),
-		new THREE.Face3(11,8,12),
-		new THREE.Face3(6,8,11),
-		new THREE.Face3(8,6,7),
-		new THREE.Face3(8,9,10),
-		new THREE.Face3(10,12,8),
-	);
-
-	geometry.computeBoundingSphere();
-
-	return geometry;
-}
-
-function i()
-{
-	//the base of i
-	var geom = new THREE.Geometry();
-
-	geom.vertices.push
-	(
-		//straight palkki
-		new THREE.Vector3(-5,20,0), //mid left 0
-		new THREE.Vector3(-5,-10,0), //down left 1
-		new THREE.Vector3(10,-10,0), //down right 2
-		new THREE.Vector3(10,20,0),  //mid right 3
-		new THREE.Vector3(10,50,0),	 //Top right 4
-		new THREE.Vector3(-5,50,0),  //top left 5
-	);
-
-	geom.faces.push
-	(
-
-		new THREE.Face3(0,1,2), //down square
-		new THREE.Face3(0,2,3), 
-
-		new THREE.Face3(1,3,5), //upper square
-		new THREE.Face3(3,4,5),
-	);
-
-	//create point for i
-	var geom2 = new THREE.CircleGeometry(8,32);
-	geom2.translate(2.5,65,0);
-	//adding meshes so we can use matrix calculations
-	var mesh = (geom, new THREE.MeshBasicMaterial({color:0x0f0f0f}));
-	var mesh2 = (geom2, new THREE.MeshBasicMaterial({color:0x0f0f0f}));
-
-	var singleGeometry = new THREE.Geometry();
-
-	singleGeometry.merge(geom,mesh.matrix);
-	singleGeometry.merge(geom2,mesh2.matrix);
-
-	return singleGeometry;
-
-}
-
-function init()
-{
-	ChangeHTMLOf('#textWindow','MainPage.html');
-	renderer = new THREE.WebGLRenderer();
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.01, 1000 );
-	camera.position.z = 130;
-	camera.position.x = 20;
-	camera.position.y = 30;
-
-
-	mesh = new THREE.Mesh(K(), new THREE.MeshBasicMaterial({color: 0x00ffff}));
-	scene = new THREE.Scene();
-
-	scene.add(mesh);
-	renderer.setSize( window.innerWidth, window.innerHeight );	
-
-	
-	canvas[0].appendChild(renderer.domElement);
-}
-
-
-function render()
-{
-	requestAnimationFrame( render );
-	mesh.rotation.y = startx * 5;
-	if(startx < -1.0)
-	{
-		increase = true;
-	}
-	
-	if(startx > 1.0)
-	{
-		increase = false;
-	}
-	
-	if(increase)
-	{
-		startx += 0.01;
-	}
-	else
-	{
-		startx -= 0.01;
-	}
-	
-
-	renderer.render( scene, camera );
-
 }
 
