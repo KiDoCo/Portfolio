26f60aad256df7e07515e0790da103de0cb28032
KiDoCo <joonastu97@gmail.com>
2019-09-07 14:16:05 +0300

    ADD a WIP mobile friendly view of website

--- Resources/JS/Loading circle.js
+++ Resources/JS/Loading circle.js
@@ -8,89 +8,75 @@ var x = 40;
 var y = 50;
 var startx = 0;
 
-function main()
-{	
+function main() {
 	//readTextFile("../Text/soos.xml");
-	gl.translate(canvas.width/2,canvas.height/2);
+	gl.translate(canvas.width / 2, canvas.height / 2);
 	gl.rotate(15 * Math.PI / 180)
-	draw();	
+	draw();
 }
 
 //event when resizes occurs
-$(window).resize(function()
-{
+$(window).resize(function () {
 	canvas.height = window.innerHeight;
 	canvas.width = window.innerWidth;
 });
 
-function render()
-{
+function render() {
 	main();
 	requestAnimationFrame(render);
 	startx += 1;
-	gl.translate(-canvas.width/2,-canvas.height/2);
-	
-	if(startx % 120 == 0 && startx != 0)
-	{
+	gl.translate(-canvas.width / 2, -canvas.height / 2);
+
+	if (startx % 120 == 0 && startx != 0) {
 		startx = 0;
-		gl.clearRect(0,0,canvas.width,canvas.height);
-			gl.resetTransform();
+		gl.clearRect(0, 0, canvas.width, canvas.height);
+		gl.resetTransform();
 	}
 
 }
 
-function draw()
-{
+function draw() {
 
-if(startx < 25)
-{
-	gl.strokeStyle = "#00FFFF"
-}
+	if (startx < 25) {
+		gl.strokeStyle = "#00FFFF"
+	}
 
-if(startx > 25 && startx < 50)
-{
-	gl.strokeStyle = "#FFFF00";
-}
+	if (startx > 25 && startx < 50) {
+		gl.strokeStyle = "#FFFF00";
+	}
 
-if(startx > 50 && startx < 75)
-{
-	gl.strokeStyle = "#FF00FF";
-}
-if(startx > 75)
-{
-	gl.strokeStyle = "#00FF00";
-}
-	gl.strokeRect(100 ,100,x,y);
+	if (startx > 50 && startx < 75) {
+		gl.strokeStyle = "#FF00FF";
+	}
+	if (startx > 75) {
+		gl.strokeStyle = "#00FF00";
+	}
+	gl.strokeRect(100, 100, x, y);
 }
 
-function save()
-{
+function save() {
 	gl.save();
 }
 
-function getLocation()
-{
-	var x = document.getElementById("XD"); 
-	if(navigator.geolocation)
-	{
+function getLocation() {
+	var x = document.getElementById("XD");
+	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(showPosition);
 		return true;
 	}
-	else
-	{
+	else {
 		x.innerHTML = "U browser no support geo, plz gime munnie";
 		return false;
 	}
 }
 
-function showPosition(position)
-{
-    var mapOptions = {
-		
-        center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    }
+function showPosition(position) {
+	var mapOptions = {
+
+		center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+		zoom: 10,
+		mapTypeId: google.maps.MapTypeId.HYBRID
+	}
 	var map = new google.maps.Map(document.getElementById("XD"), mapOptions);
 	return true;
 }
--- Resources/JS/PageGL.js
+++ Resources/JS/PageGL.js
@@ -4,30 +4,25 @@ var increase = false;
 var boxarray = [];
 var style = 16;
 var initvalue = parseFloat(style);
-ChangeHTMLOf('#textWindow','HTML/MainPage.html');
+ChangeHTMLOf('#textWindow', 'HTML/MainPage.html');
 var G_gl = document.querySelector("#glcontainer").getContext("webgl");
 main();
 
-function main()
-{
+function main() {
 
 
 
-	G_gl.clearColor(0.0,0.0,0.0,1.0);
+	G_gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	G_gl.clear(G_gl.COLOR_BUFFER_BIT);
 }
 
 
-function readTextFile(file)
-{
+function readTextFile(file) {
 	var rawFile = new XMLHttpRequest();
-	rawFile.open('GET',file,true);
-	rawFile.onreadystatechange = function()
-	{
-		if(rawFile.readyState == 4)
-		{
-			if(rawFile.status === 200 || rawFile.status == 0)
-			{
+	rawFile.open('GET', file, true);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState == 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
 				var allText = rawFile.responseText;
 				var para = document.createElement("p");
 				var node = document.createTextNode(allText);
@@ -42,33 +37,25 @@ function readTextFile(file)
 
 
 
-function resize(canvas)
-{
+function resize(canvas) {
 	var displayWidth = canvas.clientWidth;
-var displayHeight = canvas.clientHeight;
-if(canvas.width != displayWidth || canvas.height != displayHeight)
-{
-	canvas.width = displayWidth;
-	canvas.height = displayHeight;
+	var displayHeight = canvas.clientHeight;
+	if (canvas.width != displayWidth || canvas.height != displayHeight) {
+		canvas.width = displayWidth;
+		canvas.height = displayHeight;
+	}
 }
-}
-
-
-
 
 
 //event when resizes occurs
-$(window).resize(function()
-{
+$(window).resize(function () {
 
 	G_gl.height = canvas.clientHeight;
 	G_gl.width = canvas.clientWidth;
 
-	});
+});
 
-function ChangeHTMLOf(source,target)
-{
-	console.log("Load different");
+function ChangeHTMLOf(source, target) {
 	$(source).load(target);
 }
 
