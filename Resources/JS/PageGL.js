76e2d3528438f8f2bdae0b2d1364b09aa9ce0b87
KiDoCo <joonastu97@gmail.com>
2019-11-06 15:11:31 +0200

    Changed index from html to php

--- Resources/JS/PageGL.js
+++ Resources/JS/PageGL.js
@@ -1,28 +1,64 @@
-
-var startx = 0;
-var increase = false;
-var boxarray = [];
-var style = 16;
-var initvalue = parseFloat(style);
-ChangeHTMLOf('#textWindow', 'HTML/MainPage.html');
+//Canvas references
 var G_gl = document.querySelector("#glcontainer").getContext("webgl");
-main();
 
-function main() {
+var headerGL = document.querySelector("#glcontainerheader").getContext("webgl");
+
+//Three js modules load
+var scene = new THREE.Scene();
+var loader = new THREE.GLTFLoader();
+var camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,1,2000);
+camera.position.z = 100;
+scene.add(camera);
+var sword;
+var renderer = new THREE.WebGLRenderer(headerGL);
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize(window.innerWidth, window.innerHeight * 0.25);
+
+var directionalLight = new THREE.DirectionalLight( 0xffffff ,100.0);
+directionalLight.position.set( 0, 0, 0 );
+camera.add( directionalLight);		
+
+//Page initial loads
+ChangeHTMLOf('#textWindow', 'HTML/MainPage.html');
 
 
+loader.load("./Resources/Models/scene.gltf",function(gltf)
+{
+	console.log(gltf.scene);
+	gltf.scene.position.x = 0;
+	gltf.scene.position.y = 0;
+	gltf.scene.position.z = 50;
+	sword = gltf.scene.children[0];
+    scene.add(gltf.scene);
+},
+	function ( xhr ) {
 
-	G_gl.clearColor(0.0, 0.0, 0.0, 1.0);
-	G_gl.clear(G_gl.COLOR_BUFFER_BIT);
-}
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log('  An error happened' );
+
+	}
+);
+
+window.setInterval(function(){
+    
+	sword.rotation.x += 0.01;
+	sword.rotation.y += 0.05;
+    renderer.render(scene,camera);
+
+    
+},10);
 
 function readTextFile(file) {
 	var rawFile = new XMLHttpRequest();
 	rawFile.open('GET', file, true);
 	rawFile.onreadystatechange = function () {
 		if (rawFile.readyState == 4) {
-			if (rawFile.status === 200 || rawFile.status == 0) {
+			if (rawFile.status === 200 || rawFile.status === 0) {
 				var allText = rawFile.responseText;
 				var para = document.createElement("p");
 				var node = document.createTextNode(allText);
@@ -35,11 +71,14 @@ function readTextFile(file) {
 	rawFile.send(null);
 }
 
-
+function Download(url)
+{
+    document.getElementById("testFrame").src = url;
+}
 
 function resize(canvas) {
-	var displayWidth = canvas.clientWidth;
-	var displayHeight = canvas.clientHeight;
+	var displayWidth = window.innerWidth;
+	var displayHeight = window.innerHeight;
 	if (canvas.width != displayWidth || canvas.height != displayHeight) {
 		canvas.width = displayWidth;
 		canvas.height = displayHeight;
@@ -48,12 +87,7 @@ function resize(canvas) {
 
 
 //event when resizes occurs
-$(window).resize(function () {
-
-	G_gl.height = canvas.clientHeight;
-	G_gl.width = canvas.clientWidth;
-
-});
+$(window).resize(resize);
 
 function ChangeHTMLOf(source, target) {
 	$(source).load(target);
